a6c6366640b5be51e09e81c0ad37c8f7ad7af6e8
kleykovsky <kleykovsky@gmail.com>
2022-10-23 03:52:29 +0500

    rev: 1.1

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -10,12 +10,29 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=180&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res  = await this.getResource(`${this._apiBase}characters?limit=9&offset=180&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        const notDesc = 'Sorry the description hasn\'t been added yet!'
+        const amountDesc = char.description
+        const MAX_LEN = 210;
+        const newAmDesc = (amountDesc.length > MAX_LEN) ?
+              amountDesc.substring(0, MAX_LEN) + '...' : amountDesc
+        return {
+            name: char.name,
+            description: newAmDesc ? newAmDesc : notDesc,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[0].url,
+        }
     }
 }
 export default MarvelService;
